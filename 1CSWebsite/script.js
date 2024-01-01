3cab4f5d6252d09ba35c196f5134e1a7abc71fb7
Tim Wells <timwells@westtek.com>
2024-01-01 12:52:01 -0800

    clean-up script to only required functionality

--- 1CSWebsite/script.js
+++ 1CSWebsite/script.js
@@ -1,35 +1,12 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
 
-// Get the navbar
-
-
-// Get the offset position of the navbar
-
+window.onscroll = function () { navbarPin() };
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    var navbar = document.getElementById("NavBar");
-
-    var sticky = navbar.offsetTop;
-
-    if (window.pageYOffset > sticky) {
+function navbarPin() {
+    const navbar = document.getElementById("NavBar");
+    if (window.pageYOffset > navbar.offsetTop) {
         navbar.classList.add("sticky")
-        var links = document.getElementsByTagName("a");
-        console.log(links)
-        for (var i = 0; i < links.length; i++) {
-            if (links[i].href) {
-                links[i].style.color = "#000000";
-            }
-        }
-
     } else {
         navbar.classList.remove("sticky");
-        var links = document.getElementsByTagName("a");
-        for (var i = 0; i < links.length; i++) {
-            if (links[i].href) {
-                links[i].style.color = "white";
-            }
-        }
     }
 }
